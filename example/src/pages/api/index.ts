607b58b5af37af4374527189444353c0ec8d06f6
Roman Bobrovskiy <roman@dbbsoftware.com>
2024-02-20 19:28:33 +0200

    added example of nextjs app

---
+++ example/src/pages/api/index.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const titles = [
+  'Nice! You landed for AB test experience',
+  'Nice! Here is another experience',
+  'This is one more great title to read'
+]
+
+export default async function getHomePage(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({
+    title: titles[Math.floor(Math.random() * 3)]
+  })
+}
---
+++ example/src/pages/index.tsx
@@ -0,0 +1,21 @@
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:3000/api')
+    .then((r) => r.json())
+    .catch(() => ({ title: 'Pregenerated page data.' }))
+
+  return { props: res }
+}
+
+export default function Home({ title }: { title: string }) {
+  return (
+    <>
+      <h1>Welcome to Next Cache Handler</h1>
+      <h2>
+        To change page data and trigger api to get ew random text add cookie or query with key <b>abtest</b>. Cache
+        handler is going to cache and generate data for all combinations of those keys, so if users returns to the same
+        key what is already available in cache - he will retrieve cached result
+      </h2>
+      <p>{title}</p>
+    </>
+  )
+}
