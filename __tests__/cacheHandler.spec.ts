4d0277c0240b6c6609f71c14a39c26d4f34b0b7c
Roman Bobrovskiy <roman@dbbsoftware.com>
2024-02-27 20:23:39 +0200

    added more logging and updated tests

--- __tests__/cacheHandler.spec.ts
+++ __tests__/cacheHandler.spec.ts
@@ -1,14 +1,26 @@
-import { CacheHandler, FileSystemCache } from '../src'
+import { CacheHandler, FileSystemCache, BaseLogger } from '../src'
 import { mockNextHandlerContext, mockPageData, mockHandlerMethodContext, mockCacheStrategyContext } from './mocks'
+
+const mockGetData = jest.fn()
+const mockSetData = jest.fn()
+const mockDeleteData = jest.fn()
 
 jest.mock('../src/cacheStrategy/fileSystem', () => {
   return {
     FileSystemCache: jest.fn().mockImplementation(() => ({
-      get: jest.fn(),
-      set: jest.fn()
+      get: jest.fn((...params) => mockGetData(...params)),
+      set: jest.fn((...params) => mockSetData(...params)),
+      delete: jest.fn((...params) => mockDeleteData(...params))
     }))
   }
 })
+
+const mockLogger: BaseLogger = {
+  info: jest.fn(),
+  error: jest.fn()
+}
+
+const mockCacheKey = 'test'
 
 describe('CacheHandler', () => {
   beforeAll(() => {
@@ -85,7 +97,7 @@ describe('CacheHandler', () => {
     }
   ])(
     'should handle cache $description',
-    ({ cacheCookie, cacheQuery, cacheStrategy, addDeviceSplit, overrideHeaders, expectedCacheSuffix }) => {
+    async ({ cacheCookie, cacheQuery, cacheStrategy, addDeviceSplit, overrideHeaders, expectedCacheSuffix }) => {
       if (cacheCookie) {
         CacheHandler.addCookie(cacheCookie)
       }
@@ -94,6 +106,7 @@ describe('CacheHandler', () => {
       }
       CacheHandler.enableDeviceSplit = addDeviceSplit
       CacheHandler.setCacheStrategy(cacheStrategy)
+      CacheHandler.setLogger(mockLogger)
 
       const cacheHandler = new CacheHandler({
         ...mockNextHandlerContext,
@@ -102,11 +115,11 @@ describe('CacheHandler', () => {
       const pageKey = 'index'
       const currentCacheKey = [pageKey, expectedCacheSuffix].filter(Boolean).join('-')
 
-      cacheHandler.get(pageKey)
+      await cacheHandler.get(pageKey)
       expect(cacheStrategy.get).toHaveBeenCalledTimes(1)
       expect(cacheStrategy.get).toHaveBeenCalledWith(currentCacheKey, mockCacheStrategyContext)
 
-      cacheHandler.set(pageKey, mockPageData, mockHandlerMethodContext)
+      await cacheHandler.set(pageKey, mockPageData, mockHandlerMethodContext)
       expect(cacheStrategy.set).toHaveBeenCalledTimes(1)
       expect(cacheStrategy.set).toHaveBeenCalledWith(
         currentCacheKey,
@@ -120,4 +133,93 @@ describe('CacheHandler', () => {
       )
     }
   )
+
+  it('should log when read data from cache', async () => {
+    CacheHandler.setCacheStrategy(new FileSystemCache())
+    CacheHandler.setLogger(mockLogger)
+    const cacheHandler = new CacheHandler(mockNextHandlerContext)
+
+    await cacheHandler.get(mockCacheKey)
+
+    expect(mockLogger.info).toHaveBeenCalledTimes(1)
+    expect(mockLogger.info).toHaveBeenCalledWith(`Reading cache data for ${mockCacheKey}`)
+  })
+
+  it('should log when failed to read data from cache', async () => {
+    const errorMessage = 'Error reading'
+    mockGetData.mockRejectedValueOnce(errorMessage)
+
+    CacheHandler.setCacheStrategy(new FileSystemCache())
+    CacheHandler.setLogger(mockLogger)
+    const cacheHandler = new CacheHandler(mockNextHandlerContext)
+
+    await cacheHandler.get(mockCacheKey)
+
+    expect(mockLogger.info).toHaveBeenCalledTimes(1)
+    expect(mockLogger.info).toHaveBeenCalledWith(`Reading cache data for ${mockCacheKey}`)
+
+    expect(mockLogger.error).toHaveBeenCalledTimes(1)
+    expect(mockLogger.error).toHaveBeenCalledWith(`Failed read cache for ${mockCacheKey}`, errorMessage)
+  })
+
+  it('should log when set data to cache', async () => {
+    CacheHandler.setCacheStrategy(new FileSystemCache())
+    CacheHandler.setLogger(mockLogger)
+    const cacheHandler = new CacheHandler(mockNextHandlerContext)
+
+    await cacheHandler.set(mockCacheKey, mockPageData, mockCacheStrategyContext)
+
+    expect(mockLogger.info).toHaveBeenCalledTimes(1)
+    expect(mockLogger.info).toHaveBeenCalledWith(`Writing cache for ${mockCacheKey}`)
+  })
+
+  it('should log when failed to set data to cache', async () => {
+    const errorMessage = 'Error writing'
+    mockSetData.mockRejectedValueOnce(errorMessage)
+
+    CacheHandler.setCacheStrategy(new FileSystemCache())
+    CacheHandler.setLogger(mockLogger)
+    const cacheHandler = new CacheHandler(mockNextHandlerContext)
+
+    await cacheHandler.set(mockCacheKey, mockPageData, mockCacheStrategyContext)
+
+    expect(mockLogger.info).toHaveBeenCalledTimes(1)
+    expect(mockLogger.info).toHaveBeenCalledWith(`Writing cache for ${mockCacheKey}`)
+
+    expect(mockLogger.error).toHaveBeenCalledTimes(1)
+    expect(mockLogger.error).toHaveBeenCalledWith(`Failed to write cache for ${mockCacheKey}`, errorMessage)
+  })
+
+  it('should delete page cache if data was not passed', async () => {
+    CacheHandler.setCacheStrategy(new FileSystemCache())
+    CacheHandler.setLogger(mockLogger)
+    const cacheHandler = new CacheHandler(mockNextHandlerContext)
+
+    await cacheHandler.set(mockCacheKey, null, mockCacheStrategyContext)
+
+    expect(mockDeleteData).toHaveBeenCalledTimes(1)
+    expect(mockDeleteData).toHaveBeenCalledWith(mockCacheKey, mockCacheStrategyContext)
+
+    expect(mockLogger.info).toHaveBeenCalledTimes(2)
+    expect(mockLogger.info).toHaveBeenNthCalledWith(1, `Writing cache for ${mockCacheKey}`)
+    expect(mockLogger.info).toHaveBeenNthCalledWith(2, `Deleting cache data for ${mockCacheKey}`)
+  })
+
+  it('should log when failed to delete page cache data', async () => {
+    const errorMessage = 'error deleting'
+    mockDeleteData.mockRejectedValueOnce(errorMessage)
+
+    CacheHandler.setCacheStrategy(new FileSystemCache())
+    CacheHandler.setLogger(mockLogger)
+    const cacheHandler = new CacheHandler(mockNextHandlerContext)
+
+    await cacheHandler.set(mockCacheKey, null, mockCacheStrategyContext)
+
+    expect(mockLogger.info).toHaveBeenCalledTimes(2)
+    expect(mockLogger.info).toHaveBeenNthCalledWith(1, `Writing cache for ${mockCacheKey}`)
+    expect(mockLogger.info).toHaveBeenNthCalledWith(2, `Deleting cache data for ${mockCacheKey}`)
+
+    expect(mockLogger.error).toHaveBeenCalledTimes(1)
+    expect(mockLogger.error).toHaveBeenCalledWith(`Failed to delete cache data for ${mockCacheKey}`, errorMessage)
+  })
 })
