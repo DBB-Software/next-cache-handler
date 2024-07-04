0aca40e54d76b1fdaaddeb6b1fbc1292e1a539f0
DBB-trsergey <stu@dbbsoftware.com>
2024-07-04 23:11:12 +0300

    feat: improve example for app router

--- examples/app-router/src/app/page/subpage/page.tsx
+++ examples/app-router/src/app/page/subpage/page.tsx
@@ -1,7 +1,7 @@
-import { TestPage } from './../components/TestPage'
+import { TestPage } from './../../../components/TestPage'
 
 const getTitle = async () => {
-  const res = await fetch('http://localhost:3000/api', { next: { tags: ['index'] } })
+  const res = await fetch('http://localhost:3000/api', { next: { tags: ['subpage'] } })
     .then((r) => r.json())
     .catch(() => ({ title: 'Pregenerated page data.' }))
 
