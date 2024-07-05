57522cb4e773f68849966f4b3f2ca4e4fe4fd940
DBB-trsergey <stu@dbbsoftware.com>
2024-07-05 16:55:06 +0300

    chore: clean up

--- examples/page-router/cacheHandler.mjs
+++ examples/page-router/cacheHandler.mjs
@@ -1,9 +1,5 @@
-import { Cache } from '@dbbs/next-cache-handler-core'
+import { Cache, FileSystemCache } from '@dbbs/next-cache-handler-core'
 
-import { FileSystemCache } from '@dbbs/next-cache-handler-core'
 Cache.addCookie('abtest').addQuery('abtest').setCacheStrategy(new FileSystemCache()).addDeviceSplit()
-
-// import { MemoryCache } from '@dbbs/next-cache-handler-core'
-// Cache.addCookie('abtest').addQuery('abtest').setCacheStrategy(new MemoryCache()).addDeviceSplit()
 
 export default Cache
