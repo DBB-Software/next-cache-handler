ef11f11e88ee50add55d7f6310325f876dbc1db2
DBB-trsergey <stu@dbbsoftware.com>
2024-08-06 20:40:45 +0300

    fix: set ttl for redis record

--- packages/redis-cache/src/RedisStack.ts
+++ packages/redis-cache/src/RedisStack.ts
@@ -60,13 +60,11 @@ export class RedisStack implements RedisAdapter {
         : ''
     const generalTags = [headersTags, data?.tags?.join(SEPARATOR)].filter(Boolean).join(SEPARATOR)
 
-    const cacheData = {
-      ...data,
-      ...(data.revalidate && { EX: Number(data.revalidate) }),
-      currentCacheKey: cacheKey,
-      generalTags
-    }
+    const cacheData = { ...data, currentCacheKey: cacheKey, generalTags }
     await this.client.json.set(`${pageKey}//${cacheKey}`, '.', cacheData as unknown as RedisJSON)
+    if (data.revalidate) {
+      await this.client.expire(`${pageKey}//${cacheKey}`, Number(data.revalidate))
+    }
   }
 
   async findCacheKeys(tag: string, cacheKeys: string[]): Promise<string[]> {
